e6d3eed7e248561219ff91b16516742bd07176df
Anant20072007 <25116cn039@glbitm.ac.in>
2026-02-17 11:15:55 +0530

    Add files via upload

---
+++ anant1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main (){
+int x;
+scanf("%d",&x);
+if ( x == 'a' || x == 'e' || x == 'o' || x == 'u') {
+    printf("it is a vowel\n");
+} else {
+    printf("it is not a vowel\n");
+}
+return 0;
+}
+
+
+
+
+
+
+
+    
